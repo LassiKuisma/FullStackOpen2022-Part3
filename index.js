66a807ab3ebd0bfef63b27a4d48fb27cf9847d37
Lassi Kuisma <lassi.kuisma1@gmail.com>
2022-09-20 21:58:59 +0300

    handle updating existing contact

--- index.js
+++ index.js
@@ -91,6 +91,22 @@ app.post('/api/persons', (request, response) => {
     })
 })
 
+// update existing
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const contact = {
+        name: body.name,
+        number: body.number,
+    }
+
+    Contact.findByIdAndUpdate(request.params.id, contact, { new: true })
+        .then(updated => {
+            response.json(updated)
+        })
+        .catch(error => next(error))
+})
+
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
